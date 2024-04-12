3e3a960985ef1e7dc7861f3e77e789fdcbce49e5
kenneth <kennjorothuo@gmail.com>
2024-04-12 16:40:46 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  let start = 0;
+  let end = str.length - 1;
+
+  while (start < end) {
+      if (str[start] !== str[end]) {
+          return false;
+      }
+      start++;
+      end--;
+  }
+
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
+// Test cases
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("robot")); // false
+console.log(isPalindrome("level")); // true
+console.log(isPalindrome("hello")); // false
